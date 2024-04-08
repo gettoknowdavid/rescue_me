999ecb41f46f9e516b6b329eaf871cdb0bcbba6e
David Michael II <knowdavidmichael@gmail.com>
2024-04-08 22:05:22 +0100

    fix: remove supabase and env

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rescue_me/app/app.dialogs.dart';
 import 'package:stacked_themes/stacked_themes.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app.bottomsheets.dart';
 import 'app/app.dart';
 import 'app/app.locator.dart';
 import 'app/app.snackbars.dart';
-import 'core/env/env.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -16,7 +14,6 @@ Future<void> main() async {
 
   await Future.wait([
     Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
-    Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey),
     setupLocator(),
     ThemeManager.initialise(),
     setupSnackbarUI(),
