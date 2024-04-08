6ff2a5dc008a8c9283d2bae26ab4bd02c8e87873
David Michael II <knowdavidmichael@gmail.com>
2024-04-08 16:54:47 +0100

    fix: login view styling

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rescue_me/ui/common/app_styles.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked/stacked_annotations.dart';
 
+import '../../widgets/app_back_button.dart';
 import '../../widgets/auth_redirect_button.dart';
 import 'form_validator.dart';
 import 'login_form_widget.dart';
@@ -17,31 +19,27 @@ class LoginView extends StackedView<LoginViewModel> {
 
   @override
   Widget builder(context, viewModel, child) {
-    final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
-    final textTheme = theme.textTheme;
-
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: Navigator.canPop(context) ? const AppBackButton() : null,
+      ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0).r,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text('Login', style: textTheme.headlineMedium),
+            Text('Login', style: context.headline),
             4.verticalSpace,
             Text(
-              "Welcome back, you've been missed. Log in with your credentials to continue.",
-              style: textTheme.bodyMedium?.copyWith(
-                color: colorScheme.onBackground.withOpacity(0.8),
-              ),
+              "Welcome back, you've been missed. Log in with your email and password to continue.",
+              style: context.subtitleStyle,
             ),
             30.verticalSpace,
             const LoginFormWidget(),
             30.verticalSpace,
             AuthRedirectButton(
               text: "Don't have an account yet?",
-              buttonLabel: 'Create now',
+              buttonLabel: 'Register now',
               onTap: viewModel.goToRegister,
             ),
           ],
